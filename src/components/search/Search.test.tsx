d5a30cd07196f9483624c1f4c20a91b7727e9de2
Ernany <ernany.goncalves@sensedia.com>
2022-07-18 13:18:40 -0300

    deploy.sh comentado

--- src/components/search/Search.test.tsx
+++ src/components/search/Search.test.tsx
@@ -1,26 +1,26 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import { Search } from "./Search";
-import * as usePostStore from '../../hooks/usePostStore';
+import usePostStore from "../../hooks/usePostStore";
 
-const makeSut = ()=>{
-    render(<Search />);
+const makeSut = () => {
+  render(<Search />);
 };
 
+jest.mock("../../hooks/usePostStore");
+const mocked = usePostStore as unknown as jest.Mock<any>;
 
+describe("<Search /> actions test", () => {
+  test.only("Should filter after clicking search button", () => {
+    mocked.mockImplementationOnce(() => jest.fn());
 
-jest.mock("../../hooks/usePostStore", () => ({
-    setSearchText: jest.fn()
-}));
+    makeSut();
 
-describe("<Search /> actions test", ()=>{
-    test.only("Should filter after clicking search button",()=>{
-        makeSut();
-        
-        const input = screen.getByLabelText("Type for search...");
-        const text = "qui";
-        fireEvent.change(input,{target: {value: text}});
-        const button = screen.getByTestId("searchBtn");
-        fireEvent.click(button);
-        expect(usePostStore.default).toHaveBeenCalled();
-    });
-})
+    const input = screen.getByLabelText("Type for search...");
+    const text = "qui";
+    fireEvent.change(input, { target: { value: text } });
+    const button = screen.getByTestId("searchBtn");
+    fireEvent.click(button);
+    console.log(mocked.mock.results[0].value);
+    expect(mocked).toBeCalledWith(text);
+  });
+});
